544ccbd8c36d542234ccce1b11aa4a5422b7ba19
cibirastadas <you@example.com>
2021-03-14 01:03:12 +0200

    Fixed language loadPath

--- src/i18next.js
+++ src/i18next.js
@@ -1,12 +1,12 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import Backend from "i18next-http-backend";
+import Backend from "i18next-xhr-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 // don't want to use this?
 // have a look at the Quick start guide
 // for passing in lng and translations on init
-const Languages = ["en", "de", "ft", "de", "es", "it"];
+const Languages = ["en", "de", "de", "es"];
 i18n
   // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
   // learn more: https://github.com/i18next/i18next-http-backend
@@ -21,11 +21,12 @@ i18n
   .init({
     fallbackLng: "en",
     debug: true,
-    whiteList: Languages,
+    backend: {
+      loadPath: "/Template/locales/{{lng}}/{{ns}}.json",
+    },
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
-    loadPath: "/locales/{{lng}}/{{ns}}.json",
   });
 
 export default i18n;
